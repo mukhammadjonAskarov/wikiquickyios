4bb1770c75c087cd385531c53ee5ebb5919d698b
shakha <shislamove@gmail.com>
2021-04-13 12:40:38 +0500

    First commit

---
+++ WikiQuicky/WikiQuicky/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  SceneDelegate.swift
+//  WikiQuicky
+//
+//  Created by Shakhzod Bobokhonov on 4/13/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+       
+        guard let winScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: winScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+      
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+       
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+     
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+   
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+     
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
---
+++ WikiQuicky/WikiQuicky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiQuicky
+//
+//  Created by Shakhzod Bobokhonov on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
